fdf8426729cce49b51ae0fc1670fd25670740f51
mbkim95 <mbkim95@naver.com>
2021-11-26 22:47:57 +0900

    Implement main 1 tab layout

--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -38,29 +38,135 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  var _index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+    _tabController.addListener(() {
+      setState(() {
+        _index = _tabController.index;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Padding(
+              padding: EdgeInsets.all(8),
+              child: Text('일별 보기', style: TextStyle(fontSize: 18)),
+            ),
+            Padding(
+                padding: EdgeInsets.all(8),
+                child: Text('합계 보기', style: TextStyle(fontSize: 18)))
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          const dialog = AddOrderDialog();
-          OrderEntity order =
-              await showDialog(context: context, builder: (context) => dialog);
-          // TODO: DB에 저장
-        },
-        tooltip: '등록하기',
-        child: const Icon(Icons.add),
-      ),
+      body: TabBarView(controller: _tabController, children: [
+        Container(
+          child: Column(
+            children: [
+              Material(
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton(
+                              child: const Icon(Icons.arrow_left),
+                              onPressed: () {},
+                            ),
+                            ElevatedButton(
+                              child: Text('2021-11-26'),
+                              onPressed: () {},
+                            ),
+                            ElevatedButton(
+                              child: const Icon(Icons.arrow_right),
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            '합계',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          Text('중량'),
+                          Text('금액'),
+                          Text('잔고')
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          SizedBox(
+                            width: 40,
+                          ),
+                          Text('0.0kg'),
+                          Text('0원'),
+                          Text('0원')
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ListView.separated(
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: index == 0
+                          ? const EdgeInsets.only(
+                              top: 16, bottom: 4, left: 8, right: 8)
+                          : index == 30
+                              ? const EdgeInsets.only(
+                                  top: 4, bottom: 64, left: 8, right: 8)
+                              : const EdgeInsets.symmetric(
+                                  horizontal: 8, vertical: 4),
+                      child: Text('2021-11-${index + 1}'),
+                    );
+                  },
+                  separatorBuilder: (context, index) =>
+                      const Divider(color: Colors.black38),
+                  itemCount: 31,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container()
+      ]),
+      floatingActionButton: _index == 0
+          ? FloatingActionButton(
+              onPressed: () async {
+                const dialog = AddOrderDialog();
+                OrderEntity order = await showDialog(
+                    context: context, builder: (context) => dialog);
+                // TODO: DB에 저장
+              },
+              tooltip: '등록하기',
+              child: const Icon(Icons.add),
+            )
+          : null,
     );
   }
 }
